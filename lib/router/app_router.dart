5fad26bec1d8ce56afc5e7d174a03fa8a497a3bc
EndoRodrigo <endorodrigo19@gmail.com>
2026-08-27 20:25:58 -0500

    feat: se implementa la configuracion de las rutas

---
+++ lib/router/app_router.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/scream/buttons/buttons_scream.dart';
+import 'package:widgets_app/presentation/scream/carts/carts_screams.dart';
+import 'package:widgets_app/presentation/scream/home/home_screams.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: HomeScreams.name,
+      builder: (context, state) => const HomeScreams(),
+    ),
+    GoRoute(
+      path: '/buttons',
+      name: ButtonsScream.name,
+      builder: (context, state) => const ButtonsScream(),
+    ),
+    GoRoute(
+      path: '/carts',
+      name: CartsScreams.name,
+      builder: (context, state) => const CartsScreams(),
+    ),
+  ],
+);
