4a8993b1dcf5bb639400dcd9fe8fa54cfc9bfe25
EndoRodrigo <endorodrigo19@gmail.com>
2026-08-27 20:27:02 -0500

    feat: se crean las screams de botones y cart

---
+++ lib/presentation/scream/buttons/buttons_scream.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ButtonsScream extends StatelessWidget {
+  static const String name = 'button';
+  const ButtonsScream({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Hola')),
+    );
+  }
+}
