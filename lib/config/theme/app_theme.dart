daed3d93e093a2cbae940f4ee54a394ae98621e8
EndoRodrigo <endorodrigo19@gmail.com>
2026-08-27 19:25:24 -0500

    feat: se crea la configuracion de colores de la app

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({required this.selectedColor})
+      :assert(selectedColor >= 0 && selectedColor <=
+      _colorThemes.length - 1, 'El color esta entre 0 - ${_colorThemes.length}');
+
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[selectedColor],
+
+    );
+  }
+}
+
+final List<Color> _colorThemes = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+];
